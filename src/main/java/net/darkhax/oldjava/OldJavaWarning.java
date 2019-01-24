d589ae395423f06522401eb0fc963d8cc693607f
Tyler Hancock <darklime@live.ca>
2019-01-24 02:48:15 -0700

    The GUI can now be translated to other languages. #3

--- src/main/java/net/darkhax/oldjava/OldJavaWarning.java
+++ src/main/java/net/darkhax/oldjava/OldJavaWarning.java
@@ -14,19 +14,19 @@ import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "oldjava", name = "Old Java Warning", version = "@VERSION@", clientSideOnly = true, certificateFingerprint = "@FINGERPRINT@")
 public class OldJavaWarning {
     
     private static final Logger LOG = LogManager.getLogger("Old Java");
-    private static final String URL_UPDATE_JAVA = "https://gist.github.com/darkhax/2d82494a88327327cec6d5172b1db239#file-oldjava-md";
-    private static final String URL_BIT_SIZE = "https://www.java.com/en/download/faq/java_win64bit.xml#Java%20for%2064-bit";
-    private static final String INFO_UPDATE_JAVA = "<html><p>Warning: An outdated version of Java has been detected. This can cause the following issues with your game.<br>It is highly recommended to update Java to a newer version of Java 8.</p><ul><li>Poor performance and stuttering.</li><li>Crashes with newer mods.</li><li>Various security exploits.</li></ul>";
-    private static final String INFO_BIT_SIZE = "A x32 installation of Java was detected. Using the x64 version of Java is recommended.";
     private static final File LOCK = new File("stop-java-detection.txt");
     
-    public OldJavaWarning() {
+    @EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
         
         Config.syncConfigData();
         new Thread(OldJavaWarning::checkJavaVersion).start();
@@ -43,19 +43,19 @@ public class OldJavaWarning {
         // Warn users if they are not using 64bit JVM and have less than 4gb available.
         if (Config.warn32 && isJvm64bit()) {
             
-            displayWarning(INFO_BIT_SIZE, "Minecraft: Java Bit Type", URL_BIT_SIZE);
+            displayWarning(I18n.format("oldjava.notice.bit.body"), I18n.format("oldjava.notice.bit.title"), I18n.format("oldjava.url.bitinfo"));
         }
         
         // Warn users about using older versions of Java.
         if (Config.warnVersion && Config.minVersion.compareTo(System.getProperty("java.version")) > 0) {
 
-            displayWarning(INFO_UPDATE_JAVA, "Minecraft: Old Java Version", URL_UPDATE_JAVA);
+            displayWarning(I18n.format("oldjava.notice.update.body"), I18n.format("oldjava.notice.update.title"), I18n.format("oldjava.url.updateinfo"));
         }
     }
     
     private static void displayWarning(String message, String title, String url) {
         
-        final String[] options = { "Read More", "Ignore", "Stop Reminding" };
+        final String[] options = { I18n.format("oldjava.buttons.readmore"), I18n.format("oldjava.buttons.ignore"), I18n.format("oldjava.buttons.stop")};
         final int response = JOptionPane.showOptionDialog(getPopupFrame(), message, title, JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
         
         // Read More
