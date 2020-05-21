3cbde91a08981918f46db7dfbe9fc4f68ecf8d2a
Tyler Hancock <darklime@live.ca>
2020-05-21 00:08:46 -0600

    Applied formatting.

--- src/main/java/net/darkhax/oldjava/Utils.java
+++ src/main/java/net/darkhax/oldjava/Utils.java
@@ -6,97 +6,96 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 /**
- * This class contains common utilities and methods for the mod. Everything in
- * this class must be safe to access or class load from any Java environment.
+ * This class contains common utilities and methods for the mod. Everything in this class must
+ * be safe to access or class load from any Java environment.
  */
 public final class Utils {
-
-	private static final File LOCK = new File("stop-java-detection.txt");
-
-	/**
-	 * Checks if the locked file {@link #LOCK} has been created. If this returns
-	 * true the mod should completely disable itself.
-	 * 
-	 * @return Whether or not the lock file has been created.
-	 */
-	public static boolean isLocked() {
-
-		return LOCK.exists();
-	}
-
-	/**
-	 * Writes the {@link #LOCK} file which will disable the mod on future runs.
-	 */
-	public static void writeLock() {
-
-		try (FileWriter writer = new FileWriter(LOCK)) {
-
-			writer.write(
-					"If this file exists, the old java version popup will not be shown. This should not be included with modpacks by default!");
-		}
-
-		catch (final IOException e) {
-
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Checks if the JVM is 64bit. This is done by checking various properties for
-	 * the 64x flag.
-	 * 
-	 * @return Whether or not the system is a 64bit JVM.
-	 */
-	public static boolean isJvm64bit() {
-
-		final String[] propertyStrings = new String[] { "sun.arch.data.model", "com.ibm.vm.bitmode", "os.arch" };
-
-		for (final String property : propertyStrings) {
-
-			final String value = System.getProperty(property);
-
-			if (value != null && value.contains("64")) {
-
-				return true;
-			}
-		}
-
-		return false;
-	}
-	
-	public static boolean hasRam(long recommended) {
-		
-		long maxMemory = Runtime.getRuntime().maxMemory();
-		return recommended <= maxMemory;
-	}
-	
-	public static boolean compareJava(String target) {
-		
-		return target.compareTo(System.getProperty("java.version")) > 0;
-	}
-	
-	public static String getJavaVersion() {
-		
-		return System.getProperty("java.version");
-	}
-	
-	public static long getMemory() {
-		
-		return Runtime.getRuntime().maxMemory();
-	}
-	
-	public static void createWarning(String message, String title, String url, String readMore, String ignore, String stop, Path file) {
-		
-		try {
-			
-			ProcessBuilder builder = new ProcessBuilder(System.getProperty("java.home") + "/bin/java", "-jar", file.toString(), message, title, url, readMore, ignore, stop);
-			builder.inheritIO();
-			builder.start();
-		} 
-		
-		catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-	}
+    
+    private static final File LOCK = new File("stop-java-detection.txt");
+    
+    /**
+     * Checks if the locked file {@link #LOCK} has been created. If this returns true the mod
+     * should completely disable itself.
+     * 
+     * @return Whether or not the lock file has been created.
+     */
+    public static boolean isLocked () {
+        
+        return LOCK.exists();
+    }
+    
+    /**
+     * Writes the {@link #LOCK} file which will disable the mod on future runs.
+     */
+    public static void writeLock () {
+        
+        try (FileWriter writer = new FileWriter(LOCK)) {
+            
+            writer.write("If this file exists, the old java version popup will not be shown. This should not be included with modpacks by default!");
+        }
+        
+        catch (final IOException e) {
+            
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Checks if the JVM is 64bit. This is done by checking various properties for the 64x
+     * flag.
+     * 
+     * @return Whether or not the system is a 64bit JVM.
+     */
+    public static boolean isJvm64bit () {
+        
+        final String[] propertyStrings = new String[] { "sun.arch.data.model", "com.ibm.vm.bitmode", "os.arch" };
+        
+        for (final String property : propertyStrings) {
+            
+            final String value = System.getProperty(property);
+            
+            if (value != null && value.contains("64")) {
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public static boolean hasRam (long recommended) {
+        
+        final long maxMemory = Runtime.getRuntime().maxMemory();
+        return recommended <= maxMemory;
+    }
+    
+    public static boolean compareJava (String target) {
+        
+        return target.compareTo(System.getProperty("java.version")) > 0;
+    }
+    
+    public static String getJavaVersion () {
+        
+        return System.getProperty("java.version");
+    }
+    
+    public static long getMemory () {
+        
+        return Runtime.getRuntime().maxMemory();
+    }
+    
+    public static void createWarning (String message, String title, String url, String readMore, String ignore, String stop, Path file) {
+        
+        try {
+            
+            final ProcessBuilder builder = new ProcessBuilder(System.getProperty("java.home") + "/bin/java", "-jar", file.toString(), message, title, url, readMore, ignore, stop);
+            builder.inheritIO();
+            builder.start();
+        }
+        
+        catch (final IOException e) {
+            
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/net/darkhax/oldjava/minecraft/Configuration.java
+++ src/main/java/net/darkhax/oldjava/minecraft/Configuration.java
@@ -10,9 +10,9 @@ import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class Configuration {
-
+    
     private final ForgeConfigSpec spec;
-
+    
     private final BooleanValue checkJavaVersion;
     private final ConfigValue<String> minJavaVersion;
     
@@ -21,19 +21,19 @@ public class Configuration {
     private final BooleanValue checkMinMemory;
     private final ConfigValue<Integer> recommendedRam;
     
-    public Configuration () {
-
+    public Configuration() {
+        
         final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         
         // Java Version
         builder.comment("Settings related to checking for a very specific Minecraft version.");
         builder.push("java-version");
-
+        
         builder.comment("Whether or not the Java version should be checked.");
-        checkJavaVersion = builder.define("enabled", true);
+        this.checkJavaVersion = builder.define("enabled", true);
         
         builder.comment("What is the minimum version of Java to look for?");
-        minJavaVersion = builder.define("minVersion", "1.8.0_162");
+        this.minJavaVersion = builder.define("minVersion", "1.8.0_162");
         builder.pop();
         
         // JVM Arch
@@ -41,7 +41,7 @@ public class Configuration {
         builder.push("jvm-architecture");
         
         builder.comment("Should people using 32 bit Java be warned and asked to use 64 bit Java instead?");
-        check32Bit = builder.define("warn32Bit", true);       
+        this.check32Bit = builder.define("warn32Bit", true);
         builder.pop();
         
         // System Memory
@@ -49,7 +49,7 @@ public class Configuration {
         builder.push("memory");
         
         builder.comment("Should users with low RAM be warned?");
-        checkMinMemory = builder.define("warnLowMemory", true);
+        this.checkMinMemory = builder.define("warnLowMemory", true);
         
         builder.comment("The amount of recommended ram to play the pack. This is in megabytes.");
         this.recommendedRam = builder.define("recommendedRam", 1024);
@@ -58,40 +58,40 @@ public class Configuration {
         this.spec = builder.build();
     }
     
-    public ForgeConfigSpec getSpec() {
-    	
-    	return this.spec;
+    public ForgeConfigSpec getSpec () {
+        
+        return this.spec;
     }
     
-    public boolean checkJavaVersion() {
-    	
-    	return this.checkJavaVersion.get();
+    public boolean checkJavaVersion () {
+        
+        return this.checkJavaVersion.get();
     }
     
-    public String getMinJavaVersion() {
-    	
-    	return this.minJavaVersion.get();
+    public String getMinJavaVersion () {
+        
+        return this.minJavaVersion.get();
     }
     
-    public boolean check32Bit() {
-    	
-    	return this.check32Bit.get();
+    public boolean check32Bit () {
+        
+        return this.check32Bit.get();
     }
     
-    public boolean checkLowMemory() {
-    	
-    	return this.checkMinMemory.get();
+    public boolean checkLowMemory () {
+        
+        return this.checkMinMemory.get();
     }
     
-    public long getRecommenedMemory() {
-    	
-    	return this.recommendedRam.get() * 1000000L;
+    public long getRecommenedMemory () {
+        
+        return this.recommendedRam.get() * 1000000L;
     }
     
-    public void forceLoad(Path path) {
-
+    public void forceLoad (Path path) {
+        
         final CommentedFileConfig configData = CommentedFileConfig.builder(path).sync().autosave().writingMode(WritingMode.REPLACE).build();
         configData.load();
-        spec.setConfig(configData);
+        this.spec.setConfig(configData);
     }
 }
